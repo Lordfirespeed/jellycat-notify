fcf69acf58c7d0e292685711cca2ac26622d5c0a
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-20 02:44:45 +0100

    add cogs + sync command tree

--- src/main.py
+++ src/main.py
@@ -1,14 +1,32 @@
 import asyncio
+from typing import ClassVar, Type
 
 import discord
+from discord.ext import commands
 
 import config
+from cogs.subscription_commands import SubscriptionCommands
+from cogs.ping_command import PingCommand
 from utils.async_interrupt import create_interrupt_future
 
 
-class MyClient(discord.Client):
+class MyClient(commands.Bot):
+    cog_types: ClassVar[list[Type[commands.Cog]]] = [
+        PingCommand,
+        SubscriptionCommands,
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     async def on_ready(self):
         print(f"Logged on as {self.user}")
+
+    async def setup_hook(self) -> None:
+        for cog_type in self.cog_types:
+            cog = cog_type(self)
+            await self.add_cog(cog)
+        await self.tree.sync()
 
     async def close(self):
         await super().close()
@@ -20,7 +38,7 @@ intents = discord.Intents.default()
 
 async def main():
     until = create_interrupt_future()
-    async with MyClient(intents=intents) as client:
+    async with MyClient(command_prefix=None, intents=intents) as client:
         connect_future = asyncio.create_task(client.start(config.discord_app_bot_token))
         await until
     await connect_future
